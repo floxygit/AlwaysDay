e55b3314da3c65b184214792616f88ad5219471b
floxygit <floxymusic1@gmail.com>
2025-12-21 15:25:55 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -5,7 +5,28 @@ declare(strict_types=1);
 namespace floxy\AlwaysDay;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\world\WorldLoadEvent;
+use pocketmine\world\World;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener {
 
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+        // Bestehende Welten sofort auf permanenten Tag setzen
+        foreach ($this->getServer()->getWorldManager()->getWorlds() as $world) {
+            $this->setAlwaysDay($world);
+        }
+    }
+
+    public function onWorldLoad(WorldLoadEvent $event): void {
+        // Neu geladene Welten auch auf permanenten Tag setzen
+        $this->setAlwaysDay($event->getWorld());
+    }
+
+    private function setAlwaysDay(World $world): void {
+        $world->setTime(World::TIME_DAY);
+        $world->stopTime();
+    }
 }
