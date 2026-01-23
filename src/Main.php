fd4ca884f9a1775649ba0d4e7e41407f691b461e
floxygit <floxymusic1@gmail.com>
2026-01-23 06:38:38 +0100

    Refactor Main class methods for conciseness

--- src/Main.php
+++ src/Main.php
@@ -1,30 +1,14 @@
 <?php
-
-declare(strict_types=1);
-
 namespace floxy\AlwaysDay;
-
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\world\WorldLoadEvent;
 use pocketmine\world\World;
-
-class Main extends PluginBase implements Listener {
-
-    public function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
-        foreach ($this->getServer()->getWorldManager()->getWorlds() as $world) {
-            $this->setAlwaysDay($world);
-        }
+class Main extends PluginBase implements Listener{
+    public function onEnable():void{
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+        array_map(fn($w)=>$this->setAlwaysDay($w),$this->getServer()->getWorldManager()->getWorlds());
     }
-
-    public function onWorldLoad(WorldLoadEvent $event): void {
-        $this->setAlwaysDay($event->getWorld());
-    }
-
-    private function setAlwaysDay(World $world): void {
-        $world->setTime(World::TIME_DAY);
-        $world->stopTime();
-    }
+    public function onWorldLoad(WorldLoadEvent $e):void{$this->setAlwaysDay($e->getWorld());}
+    private function setAlwaysDay(World $w):void{$w->setTime(World::TIME_DAY);$w->stopTime();}
 }
